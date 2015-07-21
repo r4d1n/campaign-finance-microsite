f78cfee8764c83b255240c55fec77eb7f5df6e43
Jared Radin <jared.radin@akqa.com>
2015-07-21 11:17:58 -0700

    webpack port, organize

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var  gulp = require('gulp'),
-  shell = require('gulp-shell');
+shell = require('gulp-shell');
 
 gulp.task('server', function () {
   var nodemon = require('gulp-nodemon');
   nodemon({
-  "script": "app.js",
-  "ext": "js json es6",
-  "env": { "NODE_ENV": "development" },
-  "ignore": [
+    "script": "app.js",
+    "ext": "js jsx json es6",
+    "env": { "NODE_ENV": "development" },
+    "ignore": [
     "node_modules/**",
     "public/**",
     "src/**",
@@ -17,17 +17,15 @@ gulp.task('server', function () {
     "gulpfile.js",
     "gulpfile.es6",
     "package.json"
-  ]
-});
+    ]
+  });
 });
 
 // CLI for webpack dev
 // http://webpack.github.io/docs/webpack-dev-server.html#cli
 // gulp.task('dev-webpack', shell.task([['webpack-dev-server --quiet --port ' + config.server.webpackServerPort]]));
-gulp.task('dev-webpack', shell.task([['webpack-dev-server --color --port ' + 3000]]));
+gulp.task('dev-webpack', shell.task([['webpack-dev-server --color --port ' + 8080]]));
 
 gulp.task('webpack', shell.task(['webpack']));
 
-
-// gulp.task('default', ['server']);
 gulp.task('default', ['server', 'dev-webpack']);
