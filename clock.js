092abbd78319e8e4482ffb7079ab962c59bb9c45
Jared Radin <jared.radin@akqa.com>
2015-07-24 14:20:59 -0700

    adjust clock, image

--- clock.js
+++ clock.js
@@ -6,7 +6,7 @@ var sunlightRequest = require('./lib/sunlightRequest');
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
-  cronTime: '00 15 14 * * 0-6',
+  cronTime: '00 30 14 * * 0-6',
   onTick: function() {
     console.log('Executing Sunlight Requests');
     sunlightRequest.init();
