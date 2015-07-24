3bf095af3d68b84ac87b8cc7e2787c8aaa22a50d
Jared Radin <jared.radin@akqa.com>
2015-07-24 13:48:07 -0700

    disable client ajax for now

--- src/js/barChart/index.js
+++ src/js/barChart/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let ajax = require('superagent');
-
-ajax
-.get('/api/records/recent')
-.end(function(err, res){
-  console.log(res.body)
-});
+// let ajax = require('superagent');
+//
+// ajax
+// .get('/api/records/recent')
+// .end(function(err, res){
+//   console.log(res.body)
+// });
