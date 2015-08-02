3003ff8cdb7369ddec21ebf6c977e5d4d2b21780
Jared Radin <jared.radin@gmail.com>
2015-08-02 09:11:15 -0700

    minute changes

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,6 @@ require('../styles/main.scss');
 // client side js modules
 // import * from './barChart';
 
+// require('./barChart')
 console.log('running js in the browser')
 
-require('./barChart')
