6d804b970879b6260f991cd9533029b0217d4697
Jared Radin <jared.radin@akqa.com>
2015-08-05 11:47:31 -0700

    name formatting function

--- src/js/util.js
+++ src/js/util.js
@@ -2,7 +2,7 @@
 
 function filterByName (name, arr) {
   let regex = new RegExp(name);
-  let one = arr.filter((element) {
+  let one = arr.filter((element) => {
     return regex.exec(element);
   })
   if (!one) {
@@ -12,6 +12,22 @@ function filterByName (name, arr) {
   }
 }
 
+function formatName (name) {
+  let arr = name.split(',').reverse();
+  return arr.map((el) => {
+    el = el.trim().split('');
+    console.log(el);
+    let i = 1;
+    while (i < el.length) {
+      el[i] = el[i].toLowerCase();
+      ++i;
+    }
+    console.log(el.join(''));
+    return el.join('');
+  }).join(' ')
+}
+
 module.exports = {
-  filterByName : filterByName;
+  filterByName: filterByName,
+  formatName: formatName
 }
