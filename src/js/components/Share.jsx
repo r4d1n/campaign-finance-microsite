e4773ed8d08059340ed0cd7e96e80b391c552a5d
Jared Radin <jared.radin@akqa.com>
2015-08-06 10:50:19 -0700

    share icons layout

--- src/js/components/Share.jsx
+++ src/js/components/Share.jsx
@@ -4,11 +4,17 @@ let Share = React.createClass({
   render: function () {
     return (
       <section>
-          <div className='share-container'>
-            <div>
-              <i className='fa fa-twitter'></i>
+        <div className='share-container'>
+          <div>
+            <div className='share-text'>
+              <h3>Share</h3>
+            </div>
+            <div className='share-icons'>
+              <i className='fa fa-twitter fa-3x'></i>
+              <i className='fa fa-facebook fa-3x'></i>
             </div>
           </div>
+        </div>
       </section>
     );
   }
