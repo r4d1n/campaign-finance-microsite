c9ae263d79dd06899f81f52b0e7f9f79f5e04ba0
Jared Radin <jared.radin@akqa.com>
2015-08-13 13:20:04 -0700

    ignore scroll behavior for past year changes

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -16,6 +16,6 @@ module.exports = [
     <DefaultRoute handler={CurrentCampaign} />
     <NotFoundRoute handler={CurrentCampaign} />
     <Route name="current" path="current" handler={CurrentCampaign} />
-    <Route name="past" path="past/:year" handler={PastCampaign} />
+    <Route name="past" path="past/:year" handler={PastCampaign} ignoreScrollBehavior />
   </Route>
 ]
