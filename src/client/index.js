5d3ed08a8cbc8df98e3f06dfe19c6a5c4f606317
Jared Radin <jared.radin@gmail.com>
2015-08-16 20:57:34 -0700

    error handling for initial load

--- src/client/index.js
+++ src/client/index.js
@@ -27,22 +27,28 @@ load('api/records/latest')
 .then((body) => {
   let candidates = formatCandidates(body.current);
   let past = body.past;
-  // format past candidates TODO MOVE TO SERVER 
-  for (let year in past) {
-    past[year].sort((a,b) => {
-      return b.receipts - a.receipts;
-    })
-    for (let i = 0; i < past[year].length; i++) {
-      past[year][i].raisedString = formatDollarAmount(past[year][i].receipts);
-      past[year][i].million = formatMillionString(past[year][i].raisedString);
-      past[year][i].initials = past[year][i].name.split(' ').map((word) => {
-        return word.slice(0,1)
-      }).join('')
+  // console.log(candidates, past)
+  // format past candidates TODO MOVE TO SERVER
+  if (candidates && past) {
+
+    for (let year in past) {
+      past[year].sort((a,b) => {
+        return b.receipts - a.receipts;
+      })
+      for (let i = 0; i < past[year].length; i++) {
+        past[year][i].raisedString = formatDollarAmount(past[year][i].receipts);
+        past[year][i].million = formatMillionString(past[year][i].raisedString);
+        past[year][i].initials = past[year][i].name.split(' ').map((word) => {
+          return word.slice(0,1)
+        }).join('')
+      }
     }
+    console.log(candidates)
+    Router.run(routes, Router.HistoryLocation, function (Handler) {
+      React.render(<Handler candidates={candidates} past={past}/>, document.body);
+    });
   }
-  console.log(candidates)
-  Router.run(routes, Router.HistoryLocation, function (Handler) {
-    React.render(<Handler candidates={candidates} past={past}/>, document.body);
-  });
-  // document.getElementById('app-container'));
+})
+.catch((err) => {
+  console.log(err);
 })
